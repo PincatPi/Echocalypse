4ce64a64c370a744fd4ebe2ec1fa76c29ef517ff
PincatPi <1626510475@qq.com>
2025-03-25 02:54:06 +0800

    新增了背包UI界面

--- 3D Third-Person Controller/Assets/Scripts/Enemy/EnemyFSM/State/AIDead.cs
+++ 3D Third-Person Controller/Assets/Scripts/Enemy/EnemyFSM/State/AIDead.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Timeline.Actions;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "AIDead", menuName = "StateMachine/State/AIDead")]
